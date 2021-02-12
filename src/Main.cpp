e149f70f3777a360a1c17f9e97ed9f973f47338d
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2021-02-12 15:08:36 +0000

    Implement NN Search

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,7 @@
 #include "Main.hpp"
 
+#include "Worker.hpp"
+
 #include <array>
 #include <random>
 #include <chrono>
@@ -12,11 +14,12 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 
+#include <glm/glm.hpp>
 #include <glm/vec3.hpp>
 using glm::vec3;
 
 std::default_random_engine generator;
-std::uniform_real_distribution<float> distribution(0.0f, 100.0f);
+std::uniform_real_distribution<float> distribution(0.0f, 1000.0f);
 inline float next_rand() { return distribution(generator); }
 
 // Fibonacci Hashing
@@ -27,7 +30,7 @@ inline uint32_t calc_bucket_shift(const uint32_t bucket_count)
     return 32 - static_cast<uint32_t>(log2(bucket_count));
 }
 
-#define NUM_BUCKETS 2048
+#define NUM_BUCKETS 16384
 const float bucket_size = 0.5f;
 const uint32_t bucket_shift = calc_bucket_shift(NUM_BUCKETS);
 
@@ -36,6 +39,17 @@ inline uint32_t hash_to_index(const uint32_t hash)
     const uint32_t hash2 = hash ^ (hash >> bucket_shift);
     return (2654435769u * hash2) >> bucket_shift;
 }
+
+const vec3 bucket_offsets[8] = {
+    vec3(0, 0, 0),
+    vec3(1, 0, 0),
+    vec3(0, 1, 0),
+    vec3(1, 1, 0),
+    vec3(0, 0, 1),
+    vec3(1, 0, 1),
+    vec3(0, 1, 1),
+    vec3(1, 1, 1)
+};
 
 // Hash functions
 // Generate something to fib-hash.
@@ -102,18 +116,22 @@ struct Point
     vec3 position;
     uint32_t bucket_id = 0;
     bool found_nearest = false;
-    size_t nearest_index = 0;
+    uint32_t nearest_index = 0;
 };
 
 std::array<Point, NUM_POINTS> point_cloud_input;
 std::array<Point, NUM_POINTS> point_cloud_sorted;
-std::array<Point, NUM_POINTS> point_cloud_stage_2;
+std::array<Point, NUM_POINTS> point_cloud_final;
+
+void NNApproxSearch(uint32_t start, uint32_t step);
 
 // Sorting buckets
 
 std::array<uint32_t, NUM_POINTS>  buckets_id;
 std::array<uint32_t, NUM_BUCKETS> buckets_hash;
 std::array<uint32_t, NUM_BUCKETS> buckets_boundary;
+
+#define CONCURRENT
 
 int main(int argc, char* argv[])
 {
@@ -165,7 +183,118 @@ int main(int argc, char* argv[])
         }
     }
 
-    // Points are now sorted by hash and we have a map of where groups of ids are;
+    // Points are now sorted by hash and we have a map of where groups of ids are,
+    // now run search.
+
+#ifdef CONCURRENT
+    int threads = std::thread::hardware_concurrency();
+    WorkerGroup workers;
+
+    for (int n = 0; n < threads; ++n)
+    {
+        workers.AddWorker([=]
+        {
+            NNApproxSearch(n, threads);
+        });
+    }
+
+    workers.Resolve();
+
+#else
+    NNApproxSearch(0, 1);
+
+#endif
 
     timer_end();
+
+    // O(n) search for the closest that we found
+    float nearest_found_dist = std::numeric_limits<float>::max();
+    uint32_t nearest_found_index_0 = 0;
+    uint32_t nearest_found_index_1 = 0;
+
+    for (uint32_t i = 0; i < NUM_POINTS; i++)
+    {
+        Point& p0 = point_cloud_final[i];
+        if (p0.found_nearest)
+        {
+            Point& p1 = point_cloud_final[p0.nearest_index];
+
+            float dist = glm::length(p1.position - p0.position);
+            if (dist < nearest_found_dist)
+            {
+                nearest_found_dist = dist;
+                nearest_found_index_0 = i;
+                nearest_found_index_1 = p0.nearest_index;
+            }
+        }
+    }
+
+    std::cout << "Nearest found points: ";
+    std::cout << "#" << nearest_found_index_0;
+    std::cout << ", ";
+    std::cout << "#" << nearest_found_index_1;
+    std::cout << " distance:";
+    std::cout << nearest_found_dist;
+    std::cout << " of " << NUM_POINTS;
+    std::cout << std::endl;
+}
+
+void NNApproxSearch(uint32_t start, uint32_t step)
+{
+    // For each point
+    for (uint32_t i = start; i < NUM_POINTS; i += step)
+    {
+        const Point& b0 = point_cloud_sorted[i];
+
+        // search neighbor buckets
+        float nearest_distance = std::numeric_limits<float>::max();
+        bool nearest_found = false;
+        uint32_t nearest_index = 0;
+
+        // Iterate nearby buckets
+        for (uint32_t j = 0; j < 8; j++)
+        {
+            const uint32_t bucket_index = generate_bucket_id(
+                b0.position,
+                bucket_offsets[j]);
+
+            int32_t k = buckets_boundary[bucket_index];
+
+            // Nothing in this bucket
+            if (k == -1)
+            {
+                continue;
+            }
+
+            int32_t current_bucket_id = 0;
+
+            do
+            {
+                const Point& b1 = point_cloud_sorted[k];
+                current_bucket_id = b1.bucket_id;
+
+                if (i != k && current_bucket_id == bucket_index)
+                {
+                    const float d = glm::length(b1.position - b0.position);
+                    if (d < nearest_distance)
+                    {
+                        nearest_distance = d;
+                        nearest_index = k;
+                        nearest_found = true;
+                    }
+                }
+
+                k++;
+            }
+            while (current_bucket_id == bucket_index && k < NUM_POINTS);
+        }
+
+        point_cloud_final[i] =
+        {
+            b0.position,
+            b0.bucket_id,
+            nearest_found,
+            nearest_index
+        };
+    }
 }
